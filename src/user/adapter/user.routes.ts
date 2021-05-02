647bafc0b90c0fb64524def1c1f69cab564d72e8
luispoz <luispoz@gmail.com>
2021-05-02 08:22:24 -0600

    update: data Validation

--- src/user/adapter/user.routes.ts
+++ src/user/adapter/user.routes.ts
@@ -1,18 +1,22 @@
 import express from 'express';
+import Joi from 'joi';
 import { UserController } from './user.controller';
-import { validationUser } from './user.validator';
+import { validateInputs } from './user.validator';
 
 
 const controller = new UserController()
 
 const route = express.Router()
 
-
+ 
+const schema = Joi.object({
+    id: Joi.number().required(),
+})
 
 
 
 route.get('/', controller.list);
-route.get('/:id', validationUser, controller.listOne);
+route.get('/:id', validateInputs(schema), controller.listOne);
 route.get('/page/:page', controller.listByPage);
 route.post('/', controller.insert);
 route.put('/:id', controller.update);
--- src/user/adapter/user.validator.ts
+++ src/user/adapter/user.validator.ts
@@ -3,6 +3,23 @@ import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 
 
+
+
+
+export const validateInputs = (schema: any) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const dataToValidate = req.params;
+        const result = schema.validate(dataToValidate)
+        if(result.hasOwnProperty("error")) {
+            return res.status(411).json({status: 411, stack: result.error})
+        }
+    
+        next();  
+    }
+
+}
+
+/*
 export const validationUser = (req: Request, res: Response, next: NextFunction) => {
 
     const dataToValidate = req.params;
@@ -20,3 +37,6 @@ export const validationUser = (req: Request, res: Response, next: NextFunction) 
     
   
 };
+
+*/
+
